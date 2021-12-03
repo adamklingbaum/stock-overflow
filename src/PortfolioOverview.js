10b0ed7e49649d4e21f87b4dc19bde855e88aaae
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-12-03 16:34:28 -0500

    implement placeholder chart

--- src/PortfolioOverview.js
+++ src/PortfolioOverview.js
@@ -2,6 +2,7 @@ import { Routes, Route, useParams } from 'react-router-dom';
 import { Row, Col, Table } from 'react-bootstrap';
 
 import PortfolioSummary from './PortfolioSummary';
+import PortfolioPerformance from './PortfolioPerformance';
 
 export default function PortfolioOverview() {
   let params = useParams();
@@ -13,7 +14,9 @@ export default function PortfolioOverview() {
       </div>
       <div className="my-4">
         <Row>
-          <Col xs={8}>Left</Col>
+          <Col xs={8}>
+            <PortfolioPerformance />
+          </Col>
           <Col xs={4} className="border rounded bg-light shadow-sm">
             <PortfolioSummary />
           </Col>
---
+++ src/PortfolioPerformance.js
@@ -0,0 +1,70 @@
+import { toCurrency, toPercent, toNumber, getTextColor } from './utils';
+import { Row, Col, Table } from 'react-bootstrap';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import faker from 'faker';
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      display: false,
+      position: 'top',
+    },
+    title: {
+      display: false,
+      text: 'Chart.js Line Chart',
+    },
+  },
+  scales: {
+    y: {
+      ticks: {
+        callback: function (value) {
+          return 100 * value + '%';
+        },
+      },
+    },
+  },
+};
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.float({ min: -1, max: 2 })),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+  ],
+};
+
+export default function PortfolioSummary() {
+  return (
+    <div className="py-3">
+      <h4>Performance since inception</h4>
+      <Line options={options} data={data} />
+    </div>
+  );
+}
