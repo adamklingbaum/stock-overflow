9d760ff6012bdf16b364354c550fa0352655f723
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-12-03 13:39:45 -0500

    add row/column structure for portfolio overview

--- src/PortfolioOverview.js
+++ src/PortfolioOverview.js
@@ -1,9 +1,20 @@
 import { Routes, Route, useParams } from 'react-router-dom';
+import { Row, Col } from 'react-bootstrap';
 export default function PortfolioOverview() {
   let params = useParams();
   return (
-    <div>
-      <h2>Portfolio overview for portfolio {params.id}</h2>
+    <div className="my-3">
+      <div className="my-3">
+        <h2>Portfolio {params.id} overview</h2>
+      </div>
+      <div className="my-4">
+        <Row>
+          <Col xs={8}>Left</Col>
+          <Col xs={4} className="border-start">
+            Right
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 }
