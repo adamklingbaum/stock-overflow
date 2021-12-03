bcabe9648f3a28c2f3bb513a3222c63942d857ac
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-12-03 12:12:50 -0500

    update portfolio tabs to determine active state

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,23 +1,25 @@
 import { Nav } from 'react-bootstrap';
-import { Routes, Route, useParams, Link } from 'react-router-dom';
+import { Routes, Route, useParams, Link, useLocation } from 'react-router-dom';
 import PortfolioOverview from './PortfolioOverview';
 import PortfolioHoldings from './PortfolioHoldings';
 
 export default function Portfolio() {
   let params = useParams();
+  let pathname = useLocation().pathname.split('/').pop();
+  console.log(pathname);
   return (
     <div>
       <h1>Portfolio {params.id}</h1>
-      <Nav variant="tabs" defaultActiveKey="overview">
+      <Nav variant="tabs">
         <Nav.Item>
-          <Nav.Link eventKey="overview">
+          <Nav.Link active={pathname === 'overview'}>
             <Link to="overview" style={{ textDecoration: 'none' }}>
               Overview
             </Link>
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="holdings">
+          <Nav.Link active={pathname === 'holdings'}>
             <Link to="holdings" style={{ textDecoration: 'none' }}>
               Holdings
             </Link>
